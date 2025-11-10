9a0d9fcfa68ad82066f5a224bb8713d0292f9b12
mohammad53eu <mohammadqassas114@gmail.com>
2025-11-10 21:42:57 +0300

    initial commit for frontend

---
+++ frontend/app/components/Header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <header className="flex items-center justify-between border-amber-200 border h-20 px-3">
+      <div className="w-2xl border h-18 my-1 p-2">h</div>
+      <div className="w-2xl border h-18 my-1 mr-1 p-2">new</div>
+    </header>
+  )
+}
---
+++ frontend/app/routes/home.tsx
@@ -0,0 +1,17 @@
+import { Header } from "~/components/Header";
+import type { Route } from "./+types/home";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "Chatbot" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return (
+  <>
+    <Header />
+  </>
+  ) 
+}

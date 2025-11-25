41d6f79435fe65890663b54ff2d36029e64f37e0
mohammad53eu <mohammadqassas114@gmail.com>
2025-11-25 11:56:55 +0300

    adding conversation queries

---
+++ backend/src/database/queries/conversationQueries.ts
@@ -0,0 +1,81 @@
+import { Conversation } from "../../types/chat.types";
+import pool from "../db";
+
+// get all conversations from database
+export const getAllConversations = async (
+    user_id: string
+): Promise<Conversation[]> => {
+    const result = await pool.query(
+        `SELECT * FROM conversations
+        WHERE user_id = $1
+        ORDER BY updated_at DESC;`,
+        [user_id]
+    );
+    return result.rows;
+};
+
+// add a conversation
+export const addConversation = async (
+    user_id: string,
+    title: string,
+    settings: Record<string, any>,
+    system_prompts?: string
+): Promise<Conversation> => {
+    const result = await pool.query(
+        `INSERT INTO conversations
+        (user_id, title, system_prompts, settings)
+        VALUES ($1, $2, $3, $4)
+        RETURNING *`,
+        [user_id, title, system_prompts, settings]
+    );
+    return result.rows[0];
+};
+
+// gets a specific conversation
+export const getConversation = async (
+    user_id: string,
+    conversation_id: string
+): Promise<Conversation | null> => {
+    const result = await pool.query(
+        `SELECT * FROM conversations
+        WHERE user_id = $1 AND conversation_id = $2
+        LIMIT 1`,
+        [user_id, conversation_id]
+    );
+    return result.rows[0] || null;
+};
+
+// update the title of the conversation
+export const updateConversationTitle = async (
+    user_id: string,
+    conversation_id: string,
+    newTitle: string
+): Promise<Conversation | null> => {
+    const result = await pool.query(
+        `UPDATE conversations
+        SET title = $3,
+            updated_at = CURRENT_TIMESTAMP
+        WHERE id = $1
+          AND user_id = $2
+        RETURNING *`,
+        [conversation_id, user_id, newTitle]
+    );
+
+    if (result.rows.length === 0) return null;
+
+    return result.rows[0] as Conversation;
+};
+
+// deletes a conversation
+export const deleteConversation = async (
+    user_id: string,
+    conversation_id: string
+): Promise<boolean> => {
+    const result = await pool.query(
+        `DELETE FROM conversations
+        WHERE id = $1 AND user_id = $2`,
+        [conversation_id, user_id]
+    );
+
+    return (result.rowCount?? 0) > 0;
+};

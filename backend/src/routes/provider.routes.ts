bb312ca624add580bb5a53d8c1b5136d91d36670
mohammad53eu <mohammadqassas114@gmail.com>
2025-12-02 22:28:12 +0300

    adding providers and models routes

---
+++ backend/src/routes/provider.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { addProviderModel, deleteProviderModel, getProvider, listProviderModels, upsertProvider } from "../controllers/providersControllers";
+
+const router = Router();
+
+// router.get('/list', ) // list supported providers
+
+router.get('/config', getProvider) // get provider config
+router.post('/upsert', upsertProvider) // add/update provider
+
+router.get('/:provider/models', listProviderModels) // list available models
+router.post('/:provider/models', addProviderModel) // add a model
+router.delete('/:provider/models/:modelId', deleteProviderModel) // delete a model
+
+export default router; 

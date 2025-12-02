8660c20e7aecd471665ee2df45ad829e9786160d
mohammad53eu <mohammadqassas114@gmail.com>
2025-12-02 22:37:34 +0300

    imported providers and chat routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import authRoutes from './auth.routes';
+import chatRoutes from './chat.routes';
+import providerRoutes from './provider.routes';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
+router.use('/chat', chatRoutes);
+router.use('/provider', providerRoutes);
+
 
 export default router;
 

2d7ef94eab94510fe47cea7d459a5209a0bd4ea1
mohammad53eu <mohammadqassas114@gmail.com>
2025-12-05 15:19:28 +0300

    importing auth middleware function

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,12 +2,13 @@ import { Router } from 'express';
 import authRoutes from './auth.routes';
 import chatRoutes from './chat.routes';
 import providerRoutes from './provider.routes';
+import { authMiddleware } from '../middleware/auth.middleware';
 
 const router = Router();
 
 router.use('/auth', authRoutes);
-router.use('/chat', chatRoutes);
-router.use('/provider', providerRoutes);
+router.use('/chat', authMiddleware ,chatRoutes);
+router.use('/provider', authMiddleware, providerRoutes);
 
 
 export default router;

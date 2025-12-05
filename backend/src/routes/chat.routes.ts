335f73ffd1151b933e86998af1fc4cd60b91ab04
mohammad53eu <mohammadqassas114@gmail.com>
2025-12-05 15:18:33 +0300

    importing routes functions

--- backend/src/routes/chat.routes.ts
+++ backend/src/routes/chat.routes.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express';
+import { createConversation, getConversationDetails, listConversations, sendMessage } from '../controllers/conversationControllers';
 
 const router = Router();
 
-router.get('/conversations', );   // list user conversations
-router.post('/conversations', )   // create a new conversation
+router.get('/conversations', listConversations);  // list user conversations
+router.post('/conversations', createConversation)   // create a new conversation
 
-router.get('/conversations/:id', )    // get a specific conversation (meta data about it)
+router.get('/conversations/:id', getConversationDetails)    // get a specific conversation 
 router.patch('/conversations/:id', )  // rename a conversation
 router.delete('/conversations/:id', )  // delete a conversation
 
 router.get('/conversations/:id/messages', )  // get the messages of a conversation
-router.post('/conversations/:id/messages', )  // send a messages
+router.post('/conversations/:id/messages', sendMessage)  // send a messages
 router.delete('/conversations/:id/messages/:msgId', )  // delete a messages
 
 

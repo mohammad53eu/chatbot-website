d343c4d19d4df556b413d20fefc689be938a938e
mohammad53eu <mohammadqassas114@gmail.com>
2025-12-15 22:03:45 +0300

    adding the error handler at the end

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,6 +4,7 @@ import helmet from 'helmet';
 import cors from 'cors';
 import { getPort } from './utils/env';
 import routes from './routes';
+import { errorHandler } from './middleware/error.middleware';
 const app = express();
 
 app.use(helmet());
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use('/api', routes);
 
+app.use(errorHandler);
 const port = getPort();
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => {

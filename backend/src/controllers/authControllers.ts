9cf4602e25100ce16019788e7a5d66fa4ca17e28
mohammad53eu <mohammadqassas114@gmail.com>
2025-12-16 19:52:41 +0300

    removing try/catch

--- backend/src/controllers/authControllers.ts
+++ backend/src/controllers/authControllers.ts
@@ -7,110 +7,92 @@ import { AuthenticationError, ConflictError, DatabaseError, ValidationError } fr
 
 
 export const register = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const { email, username, password } = req.body;
+    const { email, username, password } = req.body;
 
-        if(!email || !username || !password) {
-            throw new ValidationError("Email, username, and password are required");
-        }
-
-        const sanitizedEmail = sanitizeInput(email.toLowerCase());
-        const sanitizedUsername = sanitizeInput(username);
-
-        const validation = validateRegistration(sanitizedEmail, sanitizedUsername, password);
-        if(!validation.isValid){
-            throw new ValidationError(validation.message);
-        }
-
-        if (await emailExists(sanitizedEmail)) {
-            throw new ConflictError("Email already registered");
-        }
-
-        if (await usernameExists(sanitizedUsername)) {
-            throw new ConflictError("Username already taken");
-        }
-
-        const passwordHash = await hashPassword(password);
-
-        const user = await createUser(sanitizedEmail, sanitizedUsername, passwordHash);
-
-        if(!user){
-            throw new DatabaseError();
-        }
-
-        const token = generateToken(user.id, user.email);
-
-        res.status(201).json({
-            success: true,
-            data: {
-                user: {
-                    id: user.id,
-                    email: user.email,
-                    username: user.username,
-                    avatar: user.avatar
-                },
-                token
-            }
-        });
-        
-    } catch (error) {
-        console.error('Registeration error: ', error);
-        res.status(500).json({
-            success: false,
-            error: { message: 'Registration failed. Please try again.' }
-        });
+    if(!email || !username || !password) {
+        throw new ValidationError("Email, username, and password are required");
     }
+
+    const sanitizedEmail = sanitizeInput(email.toLowerCase());
+    const sanitizedUsername = sanitizeInput(username);
+
+    const validation = validateRegistration(sanitizedEmail, sanitizedUsername, password);
+    if(!validation.isValid){
+        throw new ValidationError(validation.message);
+    }
+
+    if (await emailExists(sanitizedEmail)) {
+        throw new ConflictError("Email already registered");
+    }
+
+    if (await usernameExists(sanitizedUsername)) {
+        throw new ConflictError("Username already taken");
+    }
+
+    const passwordHash = await hashPassword(password);
+
+    const user = await createUser(sanitizedEmail, sanitizedUsername, passwordHash);
+
+    if(!user){
+        throw new DatabaseError();
+    }
+
+    const token = generateToken(user.id, user.email);
+    
+    res.status(201).json({
+        success: true,
+        data: {
+            user: {
+                id: user.id,
+                email: user.email,
+                username: user.username,
+                avatar: user.avatar
+            },
+            token
+        }
+    });    
 };
 
 
 export const login = async (req: Request, res: Response): Promise<void> => {
-    try {
-        const {email, password} = req.body;
+    const {email, password} = req.body;
 
-        if (!email || !password) {
-            throw new ValidationError("Email and password are required");
-        }
-
-        const sanitizedEmail = sanitizeInput(email.toLowerCase());
-
-        const validation = validateLogin(sanitizedEmail, password);
-
-        if (!validation.isValid) {
-            throw new ValidationError(validation.message);
-
-        }
-
-        const user = await findUserByEmail(sanitizedEmail);
-
-        if (!user) {
-            throw new AuthenticationError('Invalid email or password');
-        }
-
-        const isPasswordValid = await comparePasswords(password, user.password_hash);
-
-        if(!isPasswordValid) {
-            throw new AuthenticationError('Invalid email or password');
-        }
-
-        const token = generateToken(user.id, user.email);
-
-        res.status(200).json({
-            success: true,
-            data: {
-                user: {
-                id: user.id,
-                email: user.email,
-                username: user.username,
-                avatar: user.avatar_path,
-                },
-                token
-            }
-        });
-    } catch (error) {
-        console.error('Login error:', error);
-        res.status(500).json({
-            success: false,
-            error: { message: 'Login failed. Please try again.' }
-        });
+    if (!email || !password) {
+        throw new ValidationError("Email and password are required");
     }
+
+    const sanitizedEmail = sanitizeInput(email.toLowerCase());
+
+    const validation = validateLogin(sanitizedEmail, password);
+
+    if (!validation.isValid) {
+        throw new ValidationError(validation.message);
+    }
+
+    const user = await findUserByEmail(sanitizedEmail);
+
+    if (!user) {
+        throw new AuthenticationError('Invalid email or password');
+    }
+
+    const isPasswordValid = await comparePasswords(password, user.password_hash);
+
+    if(!isPasswordValid) {
+        throw new AuthenticationError('Invalid email or password');
+    }
+
+    const token = generateToken(user.id, user.email);
+
+    res.status(200).json({
+        success: true,
+        data: {
+            user: {
+            id: user.id,
+            email: user.email,
+            username: user.username,
+            avatar: user.avatar_path,
+            },
+            token
+        }
+    });
 };
